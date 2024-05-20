0021e7751288de9453ce5f12d4136f1427f013c6
KyiLwinOo <khant.bmruby@gmail.comkhant.bmruby@gmail.comkhant.bmruby@gmail.com>
2024-05-20 11:19:23 +0400

    add projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,12 +10,31 @@ const Projects = () => {
     >
       <h2>Projects</h2>
       <div className="project">
-        <h3>Project 1</h3>
-        <p>A mobile app developed using Flutter that allows users to track their fitness activities.</p>
+        <h3>Tai Learner</h3>
+        <p>A basic Tai language learning application developed for <a href='https://shaniit.org/'>SIIT</a> (NGO in Myanmar).</p>
+        <p>Project is still under development</p>
+        <a href='https://github.com/kyilwinoo97/tai_learner.git'>Tai Learner</a>
       </div>
       <div className="project">
-        <h3>Project 2</h3>
-        <p>A cross-platform e-commerce app with real-time chat functionality and payment integration.</p>
+        <h3>End To End Encryption Sample Project</h3>
+        <p>A mobile app developed using Flutter that allows end-to-end encryption for messaging app.</p>
+        <a href='https://github.com/kyilwinoo97/EndToEndSample.git'>EndToEndSample</a>
+      </div>
+      <div className="project">
+        <h3>Lucky</h3>
+        <p>A mobile app developed using Flutter that allows users to record their daily money transitions and payments.</p>
+        <a href='https://github.com/kyilwinoo97/Lucky.git'>Lucky</a>
+      </div>
+      <div className="project">
+        <h3>Bar Mega</h3>
+        <p>A cross-platform mini restaurant management app with mini pos functionality and table management.</p>
+        <a href='https://github.com/kyilwinoo97/bar_Mega.git'>Bar Mega</a>
+      </div>
+
+      <div className="project">
+        <h3>LocationTracker</h3>
+        <p>An android app with features of tracking user device location even app is <strong>force close</strong></p>
+        <a href='https://github.com/kyilwinoo97/LocationTracker.git'>LocationTracker</a>
       </div>
     </motion.section>
   );
